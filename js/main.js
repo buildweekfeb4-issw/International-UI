12f41ccc4d14ad892ab2eeb854e5d7a538573265
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-02-05 16:07:03 -0500

    toggle content working

---
+++ js/main.js
@@ -0,0 +1,21 @@
+class TabLink {
+    constructor(link) {
+      this.tabContent = document.querySelector(`article[data-tab='${link.dataset.tab}']`);
+      this.tabContent = new Content(this.tabContent);
+      link.addEventListener('click', () => this.linkClick() );
+    }
+    linkClick() {
+      this.tabContent.toggleContent();
+    }
+  }
+  
+  class Content {
+    constructor(tabElement) {
+      this.tabElement = tabElement;
+    }
+    toggleContent() {
+      this.tabElement.classList.toggle('change');
+    }
+  }
+  const links = document.querySelectorAll('.link');
+  links.forEach( link => new TabLink(link));
