cbbb71dcd88c905f71c0a5d57c7158ab500344b9
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-02-07 12:00:10 -0500

    final features

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 class TabLink {
-    constructor(link) {
-      this.tabContent = document.querySelector(`article[data-tab='${link.dataset.tab}']`);
-      this.tabContent = new Content(this.tabContent);
-      link.addEventListener('click', () => this.linkClick() );
-    }
-    linkClick() {
-      this.tabContent.toggleContent();
-    }
+  constructor(link) {
+    this.tabContent = document.querySelector(`article[data-tab='${link.dataset.tab}']`);
+    this.bkgroundContent = document.querySelector('.bg');
+    this.tabContent = new Content(this.tabContent);
+    this.bkgroundContent = new BkGround(this.bkgroundContent);
+    link.addEventListener('click', () => this.linkClick() );
   }
-  
-  class Content {
-    constructor(tabElement) {
-      this.tabElement = tabElement;
-    }
-    toggleContent() {
-      this.tabElement.classList.toggle('change');
-    }
+  linkClick() {
+    this.tabContent.toggleContent();
+    this.bkgroundContent.toggleContent2();
+  }
+}
+
+class Content {
+constructor(tabElement) {
+  this.tabElement = tabElement;
+}
+toggleContent() {
+  this.tabElement.classList.toggle('change');
+  }
+}
+
+class BkGround {
+  constructor(tab) {
+      this.tab = tab;
   }
 
- 
+toggleContent2() {
+  this.tab.classList.toggle('change2');
+  }
+}
+
   const links = document.querySelectorAll('.link');
   links.forEach( link => new TabLink(link));
